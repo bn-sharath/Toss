ab1edb77964e64618631beb809d7472bbb9d6557
bn-sharath <sharathbn2001@gmail.com>
2021-05-23 17:03:28 +0530

    first stage screen

--- app/src/main/java/com/sharathbn2001gmail/toss/MainActivity.java
+++ app/src/main/java/com/sharathbn2001gmail/toss/MainActivity.java
@@ -13,15 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = (ImageView) findViewById(R.id.simple1);
 
-        image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"hello how it is ?...",Toast.LENGTH_SHORT).show();
 
-            }
-        });
 
     }
 
