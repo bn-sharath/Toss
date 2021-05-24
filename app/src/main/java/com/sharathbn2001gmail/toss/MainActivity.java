c1bc578288eeb3adb37c417a1ef549c248f29ab5
bn-sharath <sharathbn2001@gmail.com>
2021-05-24 20:11:13 +0530

    not so good but little better in buttons

--- app/src/main/java/com/sharathbn2001gmail/toss/MainActivity.java
+++ app/src/main/java/com/sharathbn2001gmail/toss/MainActivity.java
@@ -6,27 +6,44 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView coin_image;
-    Button ahead_button,atail_button,bhead_button,btail_button;
+     Button ahead_button,atail_button,bhead_button,btail_button;
     TextView resultA,resultB,imagecenter_toss;
 
+    int color,initial_color,red,green;
+    boolean ahead,atail,bhead,btail;
+    public MainActivity()
+    {
+        green=0xFF00FF00;
+        red=0xFFFF0000;
+        color= 0xFFFB8C00;
+      initial_color = 0xFFFFBB33;
+       ahead=false ;
+       atail=false;
+       bhead=false;
+       btail=false;
+    }
     //variables for initialing the head and tail
-    public boolean ahead ,atail,bhead,btail;
 
-    int color;
-    protected void setButtonColors(int i_color){
+
+
+    protected void setButtonColors(){
         if(ahead)
         {
-            ahead_button.setBackgroundColor(i_color);
-            btail_button.setBackgroundColor(i_color);
+            ahead_button.setBackgroundColor(color);
+            btail_button.setBackgroundColor(color);
+            bhead_button.setBackgroundColor(initial_color);
+            atail_button.setBackgroundColor(initial_color);
         }
-        else {
-            bhead_button.setBackgroundColor(i_color);
-            atail_button.setBackgroundColor(i_color);
+        else if(bhead){
+            bhead_button.setBackgroundColor(color);
+            atail_button.setBackgroundColor(color);
+            ahead_button.setBackgroundColor(initial_color);
+            btail_button.setBackgroundColor(initial_color);
         }
+
     }
 
 
@@ -34,19 +51,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         //textview.... initialising textview object to corresponding variable
-            imagecenter_toss=(TextView)findViewById(R.id.tossing);
-            resultB=(TextView)findViewById(R.id.result_b);
-            resultA=(TextView)findViewById(R.id.result_a);
+            imagecenter_toss= findViewById(R.id.tossing);
+            resultB= findViewById(R.id.result_b);
+            resultA= findViewById(R.id.result_a);
 
         //textview.... initializing button object to corresponding variable
-            ahead_button=(Button)findViewById(R.id.ahead);
-            atail_button=(Button)findViewById(R.id.atail);
-            bhead_button=(Button)findViewById(R.id.bhead);
-            btail_button=(Button)findViewById(R.id.btail);
+            ahead_button= findViewById(R.id.ahead);
+            atail_button= findViewById(R.id.atail);
+            bhead_button= findViewById(R.id.bhead);
+            btail_button= findViewById(R.id.btail);
 
         //textview.... initializing imageview object to corresponding variable
-            coin_image=(ImageView)findViewById(R.id.coin);
+            coin_image= findViewById(R.id.coin);
+
 
             // when button is clicked ,methods for what to do
         ahead_button.setOnClickListener(new View.OnClickListener() {
@@ -56,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
                 atail=false;
                 bhead=false;
                 btail=true;
-                color= 0xFFFB8C00;
-               setButtonColors(color);
+//                color= 0xFFFB8C00;
+              setButtonColors();
             }
         });
 
@@ -68,8 +88,8 @@ public class MainActivity extends AppCompatActivity {
                 btail=false;
                 atail=true;
                 ahead=false;
-                color= 0xFFFB8C00;
-                setButtonColors(color);
+//                color= 0xFFFB8C00;
+                setButtonColors();
             }
         });
 
@@ -80,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
                 ahead=false;
                 btail=false;
                 bhead=true;
-                color= 0xFFFB8C00;
-                setButtonColors(color);
+//                color= 0xFFFB8C00;
+                setButtonColors();
 
             }
         });
@@ -93,14 +113,22 @@ public class MainActivity extends AppCompatActivity {
                 bhead=false;
                 ahead=true;
                 atail=false;
-                color= 0xFFFB8C00;
-                setButtonColors(color);
+//                color= 0xFFFB8C00;
+                setButtonColors();
 
             }
         });
 
+        imagecenter_toss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagecenter_toss.setText("");
+                coin_image.animate().setDuration(2000);
+                coin_image.animate().rotationXBy(360f);
+                imagecenter_toss.setText("Head");
+            }
 
-
+        });
 
 
     }
